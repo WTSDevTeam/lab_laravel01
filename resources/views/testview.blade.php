cd223ac9bbf4922849b8323e1f61168e8c8e087f
WTSDevTeam <tyod@hotmail.com>
2021-08-13 11:37:16 +0700

    upate age textbox

--- resources/views/testview.blade.php
+++ resources/views/testview.blade.php
@@ -12,14 +12,17 @@
 </head>
 <body>
     <h1>{{ $title_name }}</h1>
-    <h2>{{ $title_name2 }}</h2>
     
     <form action="{{URL::to('getdata')}}" method="POST">
         @csrf
         <label for="">ชื่อ</label>
         <input type="text" name="txtname" id="name" value="{{ $name }}">
+        <br>
         <label for="">นามสกุล</label>
         <input type="text" name="surname" id="surname" value="{{ $sur_name }}">
+        <br>
+        <label for="">อายุ</label>
+        <input type="text" name="age" id="age" value="">
         <button type="button" onclick="hello();">button 1</button>
         <button>submit</button>
     </form>
